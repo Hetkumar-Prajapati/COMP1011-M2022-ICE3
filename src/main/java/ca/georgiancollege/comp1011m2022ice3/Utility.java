40a575da96ebd73b87e1438b5e35c6a8080b9e58
hetkumar1704 <200476049@student.gerogianc.on.ca>
2022-05-27 15:36:51 -0400

    converted utility to instance

--- src/main/java/ca/georgiancollege/comp1011m2022ice3/Utility.java
+++ src/main/java/ca/georgiancollege/comp1011m2022ice3/Utility.java
@@ -1,28 +1,42 @@
 package ca.georgiancollege.comp1011m2022ice3;
 
-/**
- *
- * Name - Diya Diya
- * Student number - 200489015
- * Course - COMP 1011
- * Semester - 3
- *
- * */
+
 public class Utility
 {
+
+    // step 1: create a private static instance member
+    private static Utility m_instance = null;
+
+    // step 2: make default constructor private.
+    private Utility() {}
+
+    // step 3: create a public static access methoad taht returns an instrance of the classs.
+    public static Utility Instance()
+    {
+//    step 4: insure that instance member variable is not been instanciated (is null)
+        if (m_instance == null)
+        {
+//        step 5: craete instance of the classs n save  reference in private variable
+            m_instance = new Utility();
+        }
+//    step 6 :  return an instance
+        return m_instance;
+    }
+
+
     /*
      * This method returns the distance from start to end
      * @param start = start vector2D
      * @param end = ending vector2D
      * */
-    public static float distance(Vector2D start, Vector2D end)
+    public float distance(Vector2D start, Vector2D end)
     {
         float diffXs = end.getX() - start.getX();
         float diffYs = end.getY() - start.getY();
         return (float) Math.sqrt(diffXs * diffXs + diffYs * diffYs);
     }
 
-    public static float distance(float x1, float y1, float x2, float y2)
+    public float distance(float x1, float y1, float x2, float y2)
     {
         float diffXs = x2 - x1;
         float diffYs = y2 - y1;
