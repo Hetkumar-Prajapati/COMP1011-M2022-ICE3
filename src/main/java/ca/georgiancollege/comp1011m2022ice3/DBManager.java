f32f3aa6d9241ce0f0fc3444749b98395b01dc52
hetkumar1704 <200476049@student.gerogianc.on.ca>
2022-05-28 16:49:04 -0400

    getter and setter for id

--- src/main/java/ca/georgiancollege/comp1011m2022ice3/DBManager.java
+++ src/main/java/ca/georgiancollege/comp1011m2022ice3/DBManager.java
@@ -1,5 +1,7 @@
 package ca.georgiancollege.comp1011m2022ice3;
 
+import java.sql.*;
+
 public class DBManager
 {
     //=================================================================
@@ -20,9 +22,36 @@ public class DBManager
     private String m_password = "123456";
     private String m_connectURl = "jdbc:mysql://loacalhost:3306/comp1011m2022";
 
-    public int insertVector2D(Vector2D vector2d)
-    {
+    public int insertVector2D(Vector2D vector2d) throws SQLException {
         int vectorID =-1;
+        ResultSet resultSet = null;
+
+        // create querry
+        String sql = "INSERT INTO vectors(X,Y) VALUES (?,?);";
+
+        try (Connection connection = DriverManager.getConnection(m_connectURl, m_user, m_password);
+             PreparedStatement statement= connection.prepareStatement(sql, new String[]{"vectorID"});
+            )
+        {
+            statement.setFloat(1, vector2d.getX());
+            statement.setFloat(2, vector2d.getY());
+
+            resultSet = statement.getGeneratedKeys();
+            while(resultSet.next())
+            {
+                vectorID = resultSet.getInt(1);
+            }
+        }
+        catch (Exception e)
+        {
+
+        }
+        finally {
+            if(resultSet!=null)
+            {
+                resultSet.close();
+            }
+        }
         return vectorID;
     }
 
